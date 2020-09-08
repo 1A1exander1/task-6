fa1a665e2054178862a9c81bb5743f9921f42fb3
1A1exander1 <xalexandr92@mail.ru>
2020-09-08 14:26:53 +0300

    task

---
+++ smartgrid.js
@@ -0,0 +1,30 @@
+module.exports = {
+    outputStyle: 'scss',
+	columns: 12,
+	offset: "15px",
+	
+	container: {
+        maxWidth: "1440px",
+        minWidth: "500px",  
+        fields: "30px"
+    },
+	breakPoints: {
+		xl: {
+            width: "1200px",
+            fields: "15px"
+        },
+      
+        lg: {
+            width: "992px"
+        },
+      
+        md: {
+            width: "768px"
+        },
+        sm: {
+            width: "576px"
+        },
+
+	},
+
+};
---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+
+$("select").on("click" , function() {
+  
+  $(this).parent(".select-box").toggleClass("open");
+  
+});
+
+jQuery(document).ready(function(){
+  jQuery('.scrollbar-inner').scrollbar();
+});
+
+
+
+
